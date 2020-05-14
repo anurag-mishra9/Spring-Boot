a9bc8758da7d0691d615d682f8efc61200483e98
anurag-mishra9 <am91294@gmail.com>
2020-05-14 10:29:21 +0800

    Commit added

--- src/main/java/com/anurag/ReservationsApplication.java
+++ src/main/java/com/anurag/ReservationsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReservationsApplication {
 
+	// Reservations Application for Hotel Room Booking
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationsApplication.class, args);
 	}
